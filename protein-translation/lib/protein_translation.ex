337a55e382eb27dbd1f5f2c3abaa3ef254d174a7
venir.dev <luca@venir.dev>
2024-03-16 02:06:06 +0100

    exercism protein translation

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,47 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
+  def of_rna(rna) do
+    list = split_rna(rna)
+    do_of_rna(list, [])
+  end
+
+  defp do_of_rna([], acc), do: {:ok, Enum.reverse(acc)}
+
+  defp do_of_rna([head | rest], acc) do
+    case head |> of_codon() do
+      {:ok, "STOP"} -> {:ok, Enum.reverse(acc)}
+      {:ok, protein} -> do_of_rna(rest, [protein | acc])
+      {:error, _} -> {:error, "invalid RNA"}
+    end
+  end
+
+  defp split_rna(rna) do
+    rna |> String.split("", trim: true) |> Enum.chunk_every(3) |> Enum.map(&Enum.join/1)
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+  """
+  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def of_codon("UGU"), do: {:ok, "Cysteine"}
+  def of_codon("UGC"), do: {:ok, "Cysteine"}
+  def of_codon("UUA"), do: {:ok, "Leucine"}
+  def of_codon("UUG"), do: {:ok, "Leucine"}
+  def of_codon("AUG"), do: {:ok, "Methionine"}
+  def of_codon("UUU"), do: {:ok, "Phenylalanine"}
+  def of_codon("UUC"), do: {:ok, "Phenylalanine"}
+  def of_codon("UCU"), do: {:ok, "Serine"}
+  def of_codon("UCC"), do: {:ok, "Serine"}
+  def of_codon("UCA"), do: {:ok, "Serine"}
+  def of_codon("UCG"), do: {:ok, "Serine"}
+  def of_codon("UGG"), do: {:ok, "Tryptophan"}
+  def of_codon("UAU"), do: {:ok, "Tyrosine"}
+  def of_codon("UAC"), do: {:ok, "Tyrosine"}
+  def of_codon("UAA"), do: {:ok, "STOP"}
+  def of_codon("UAG"), do: {:ok, "STOP"}
+  def of_codon("UGA"), do: {:ok, "STOP"}
+  def of_codon(_), do: {:error, "invalid codon"}
+end
